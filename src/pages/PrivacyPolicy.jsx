40d5c35c93a3b1902ef7699d232d22852b5decce
jasminTUM <ge49nuw@mytum.de>
2025-06-24 16:46:58 +0200

    Add Privacy plocy text

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,8 +1,101 @@
 export default function PrivacyPolicy() {
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>Privacy Policy (Terms and Conditions)</h1>
-      <p>This is the privacy policy content.</p>
+      <h1>TUM.ai Privacy Policy / Data Agreement</h1>
+
+      <p>
+        This data agreement will explain why and how our organization uses personal data we collect from TUM.ai members. If you agree with our privacy policy, please sign the document at the bottom.
+      </p>
+
+      <p>The organization in the following called TUM.ai is located at:</p>
+
+      <address>
+        TUM.ai e.V.<br />
+        Arcisstraße 21<br />
+        80333 München
+      </address>
+
+      <p>
+        The organization's data protection officer can be contacted by e-mail to the following address: <a href="mailto:contact@tum-ai.com">contact@tum-ai.com</a>
+      </p>
+
+      <p>
+        TUM.ai will notify you about any changes made to this privacy policy / data agreement 30 days before they become effective.
+      </p>
+
+      <h2>What is the legal basis for our data processing?</h2>
+      <p>
+        The legal basis for processing your data is your accession to TUM.ai. Our data processing is based on the DSGVO Art. 6 Abs. 1 S. 1 lit. b) (German: Datenschutzgrundverordnung). Data processing in the sense of the DSGVO (Art. 4 S. 2) means collection, recording, organization, arrangement, storage, adaptation or alteration, retrieval, consultation, use, disclosure by transmission, dissemination or otherwise making available, alignment or combination, restriction, erasure or destruction of data.
+      </p>
+
+      <h2>Why do we collect personal data?</h2>
+      <p>
+        TUM.ai collects your personal data to enable our organization’s purpose manifested in the associations statues §3 (Zweck des Vereins). Therefore, TUM.ai collects data in accordance with DSGVO Art. 6 Abs. 1 S. 1 lit. b) to fulfill the associations purpose. The most important ones are the following:
+      </p>
+
+      <ul>
+        <li>
+          TUM.ai aims to connect and educate people interested in artificial intelligence and related topics. Therefore, we provide our community information about the members to enable and encourage an easy exchange.
+        </li>
+        <li>
+          TUM.ai is enabled to better target potential new members and potential sponsors or partners. This includes giving sponsors the possibility to send information, opportunities and job offers to you via TUM.ai. TUM.ai will always be in between sponsors and personal e-mail addresses. To finance TUM.ai activities, we offer sponsors the possibility to spread information in our community via the management team.
+        </li>
+        <li>
+          TUM.ai pursues the goal of promoting an active association life through means of publishing social event photos for example. Such content is published with explicit consent in accordance with Art. 6 Abs. 1 S. 1 lit. a).
+        </li>
+      </ul>
+
+      <h2>What data do we collect?</h2>
+      <p>
+        TUM.ai collects the following data, your full name, course of studies and e-mail address for affairs connected to your TUM.ai membership, based on DSGVO Art. 6 Abs. 1 lit. b). Additionally, you can voluntarily provide a photo of you to be shown on our platforms and website, based on DSGVO Art. 6 Abs. 1 lit. a). Providing a photo is explicitly not mandatory and will not hinder your accession to TUM.ai. Moreover, TUM.ai stores data that you may actively and voluntarily provide in our online work- and communication spaces (i.e. chat messages, replies to surveys, etc.), based on DSGVO Art. 6 Abs. 1 lit. a).
+      </p>
+
+      <h2>How do we use your data?</h2>
+      <p>
+        TUM.ai uses your e-mail address to contact you regarding matters involving TUM.ai. This e-mail address will also be your access to TUM.ai's online work- and communication spaces, based on DSGVO Art. 6 Abs. 1 lit. b). Further, TUM.ai displays your full name, course of studies, TUM.ai internal position and team affiliation on its webpage, based on DSGVO Art. 6 Abs. 1 lit. a). Additionally, you can provide a photo and choose to have it shown on TUM.ai's website in connection with the aforementioned information, based on DSGVO Art. 6 Abs. 1 lit. a). TUM.ai points out that the information on the website is globally accessible and further use by third parties cannot be excluded. If you request the deletion of your photo on the website, TUM.ai cannot ensure that the photo was copied or manipulated by third parties. TUM.ai cannot be held responsible for the nature and form of use by third parties. Furthermore, despite revocation of your photos, TUM.ai may take photos in the context of participation in public events of the association and publish them for public relations purposes, based on DSGVO Art. 6 Abs. 1 lit. a). Further, your name and members number will be used internally in our accountant program Buchhandlungsbutler to manage TUM.ai's membership fees and possible other financial ties between you and TUM.ai, based on DSGVO Art. 6 Abs. 1 lit. b).
+      </p>
+
+      <h2>What services do we share your data with?</h2>
+      <p>
+        We share your information with a limited number of communication service providers who process it on our behalf to enable our work- and communication platforms, based on DSGVO Art. 6 Abs. 1 lit. b). Your e-mail address will be shared with Mailchimp that TUM.ai uses for mail traffic control. The main platform for knowledge storage, project management and note-taking is Notion, TUM.ai will share your mail with the service in order to create an account for you. For internal communication TUM.ai uses Slack. TUM.ai will share your e-mail address with Slack to create an account that you can join our channels with. Moreover, TUM.ai displays your full name, course of studies, TUM.ai internal position and team affiliation and if you want a photo of you in the official TUM.ai webpage (as described in "How do we use/share your data?").
+      </p>
+
+      <h2>How do we store your data?</h2>
+      <p>
+        TUM.ai shares your data as outlined in "What services do we share your data with?” with the service providers to create TUM.ai's work- and communication spaces. Any data additionally entered by you in TUM.ai's online work- and communication spaces is stored via our service partners and the service providers terms & conditions apply.
+      </p>
+
+      <h2>How long do we store your data?</h2>
+      <p>
+        TUM.ai will delete your data in accordance with German law. This means that as soon as the purpose of the data processing is no longer given, your data will be deleted. An exception to this is data with fiscal significance. According to German law, this data must be stored for ten years and will only be deleted after this period. This means if you leave TUM.ai, TUM.ai will delete your data without fiscal significance immediately and all data with fiscal significance after ten years.
+      </p>
+
+      <h2>What are your data protection rights?</h2>
+      <p>We would like to ensure that you are fully aware of all your data protection rights. Every member is entitled to the following:</p>
+
+      <ul>
+        <li>The right to access according to DSGVO Art. 15. You have the right to request a copy of your personal data from TUM.ai. The copy must be provided by TUM.ai free of charge. Only in case of obviously unfounded or excessive requests, TUM.ai may charge you a fee for this service.</li>
+        <li>The right to correction according to DSGVO Art. 16. You have the right to request that TUM.ai correct any information you believe is inaccurate. You also have the right to request TUM.ai to complete the information you believe is incomplete.</li>
+        <li>The right to erasure according to DSGVO Art. 17. You have the right to request that TUM.ai erase your personal data, under certain conditions. The right of erasure does not affect the lawfulness of the processing prior to the erasure, if processing consent was based on DSGVO Art. 6 Abs. 1 S. 1 lit. a).</li>
+        <li>The right to restrict processing according to DSGVO Art. 18. You have the right to request that TUM.ai restrict the processing of your personal data, under certain conditions.</li>
+        <li>The right to data portability according to DSGVO Art. 20. You have the right to request that TUM.ai transfer the data that we have collected to another organization, or directly to you, under certain conditions.</li>
+        <li>The right to object to processing according to DSGVO Art. 21. You have the right to object to TUM.ai’s processing of your personal data, under certain conditions.</li>
+        <li>The right to complain to a supervisory authority according to DSGVO Art. 77. You have the right to file a complaint with the authorities if you believe that TUM.ai is in breach of the DSGVO or your rights.</li>
+      </ul>
+
+      <p>
+        If you make a request, TUM.ai has one month to respond. If you would like to exercise any of these rights, please contact us through the contact details above.
+      </p>
+
+      <h2>What happens if you do not agree with the TUM.ai Privacy Policy and do not sign?</h2>
+      <p>
+        If you do not sign TUM.ai's privacy policy, TUM.ai unfortunately cannot let you participate in its online work- and communication spaces without agreeing to the privacy policy and data agreement. Because these tools are fundamental to TUM.ai’s daily operation and community, membership is consequently not possible.
+      </p>
+
+      <h2>Written declaration of consent</h2>
+      <p>
+        I agree that the aforementioned data can be used by the association for association purposes and can be forwarded to other members of the association. I consent to processing of my personal data for the other aforementioned purposes, in accordance with DSGVO Art. 6 S. 1 lit. a). I am aware that consent with the data processing is voluntary and can be revoked by me at any time in whole or in part with effect for the future.
+      </p>
     </div>
-  )
+  );
 }
